768f00af02eecc9ba109f901eb97c02584658fca
Kelly Mimms <kkmimms@gmail.com>
2023-02-14 23:00:15 -0500

    first commit

---
+++ knowledge check_kellymimms.py
@@ -0,0 +1,8 @@
+print('hello world!')
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+print(my_list [2])
+my_dict = {'apple': 1, 'banana': 2}
+print(my_dict['apple'])
+my_tuple = (1, 2, 3, 4)
+print(my_tuple[1])
